8eecfb6df6321baaf2bdf2ce2cabb1ae9a857d3f
gcfu0310 <gcfu0310@gmail.com>
2026-07-13 12:28:05 +0800

    2026-7-13-review:moveZeros

---
+++ 283.移动零/2026-7-13.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        non_zero = 0
+        for cur in range(len(nums)):
+            if nums[cur] != 0:
+                nums[non_zero],nums[cur] = nums[cur],nums[non_zero]
+                non_zero += 1
+            
