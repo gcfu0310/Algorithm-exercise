f0d91451b8a0bf5c8870cb000cd092bcf2cad8d9
gcfu0310 <gcfu0310@gmail.com>
2026-08-08 14:11:05 +0800

    方法2:将空间复杂度降低至O(1)

--- 238.除了自身以外数组的乘积/2026-8-8.py
+++ 238.除了自身以外数组的乘积/2026-8-8.py
@@ -1,7 +1,7 @@
 from typing import List
 class Solution:
     # 与接雨水一致的方法
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+    def productExceptSelf_1(self, nums: List[int]) -> List[int]:
         n = len(nums)
         left = [1] * n
         right = [1] * n
@@ -11,4 +11,16 @@ class Solution:
             right[n-i-1] = right[n-i] * nums[n-i]
         for i in range(n):
             ans.append(left[i]*right[i])
+        return ans
+
+    # 输出不算在空间复杂度中，所以直接用ans数组存放左乘积，然后右乘积就进一步跟着在ans上做更新就行
+    def productExceptSelf_2(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [1] * n
+        right = 1
+        for i in range(1,n):
+            ans[i] = ans[i-1] * nums[i-1]
+        for i in range(1,n):
+            right *= nums[n-i]
+            ans[n-i-1] = right * ans[n-i-1]
         return ans
