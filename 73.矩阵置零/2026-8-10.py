ed921829df9d055cc62178d3ac83ffd5d1496626
gcfu0310 <gcfu0310@gmail.com>
2026-08-10 10:55:56 +0800

    方法二在方法一的基础上，空间复杂度降低到O(1)

--- 73.矩阵置零/2026-8-10.py
+++ 73.矩阵置零/2026-8-10.py
@@ -20,3 +20,32 @@ class Solution:
             for j in range(n):
                 if i in set_row or j in set_column:
                     matrix[i][j] = 0
+
+    # 时间复杂度是O(mn),空间复杂度是O(1)
+    def setZeroes_2(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        Flag_c = False
+        for i in range(m):
+            if matrix[i][0] == 0:
+                Flag_c = True
+            for j in range(1,n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+        if matrix[0][0] == 0:
+            for j in range(n):
+                matrix[0][j] = 0
+
+        if Flag_c:
+            for i in range(m):
+                matrix[i][0] = 0
