12c255163270b0ea0a45e93e9b61d0d0d7d79277
gcfu0310 <gcfu0310@gmail.com>
2026-09-14 16:42:30 +0800

    new version:use tuple to contain min

--- 155.最小栈/2026-09-14.py
+++ 155.最小栈/2026-09-14.py
@@ -34,3 +34,27 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, value: int) -> None:
+        if not self.stack:
+            self.stack.append((value,value))
+        else:
+            self.stack.append((value,min(self.stack[-1][-1],value)))
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+        
+
+    def getMin(self) -> int:
+        return self.stack[-1][-1]
