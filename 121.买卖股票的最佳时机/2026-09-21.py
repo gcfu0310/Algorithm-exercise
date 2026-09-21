32b0a029b2d13d0dd33531160333458fc3f21ab1
gcfu0310 <gcfu0310@gmail.com>
2026-09-21 15:24:51 +0800

    code and note:maxprofit

---
+++ 121.买卖股票的最佳时机/2026-09-21.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        n = len(prices)
+        max_profit = 0
+        min_price = prices[0]
+        for i in range(1,n):
+            profit = prices[i]-min_price
+            if profit>max_profit:
+                max_profit = profit
+            if prices[i]<min_price:
+                min_price = prices[i]
+        return max_profit
